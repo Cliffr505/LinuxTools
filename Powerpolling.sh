89ad0782ae0fb5083b8c0e96ca711f9a734ea263
Cliffr505 <46944678+Cliffr505@users.noreply.github.com>
2024-04-30 11:15:31 -0400

    Create Powerpolling.sh

---
+++ Powerpolling.sh
@@ -0,0 +1,34 @@
+#!/bin/bash                                                                     
+                                                                                
+### CSV Creation, Interval of recording, and Number of 'INTERVALS' by the total recordings wanted.
+OUTPUT_FILE="pdu_power_usage.csv"                                               
+INTERVAL=30                                                                     
+DURATION=$((10 * INTERVAL))                                                     
+                                                                                
+### Function to extract power data from the PDU. Adjust the SNMP String and IP Address if needed
+get_a_power() {                                                                 
+   snmpwalk -v 2c -c HPCOps 172.30.61.32 SNMPv2-SMI::enterprises.21239.5.2.3.1.1.9.1 | awk -F'=' '{print $2}' | awk -F: '{print $2}'
+}                                                                               
+                                                                                
+get_b_power() {                                                                 
+   snmpwalk -v 2c -c HPCOps 172.30.61.33 SNMPv2-SMI::enterprises.21239.5.2.3.1.1.9.1 | awk -F'=' '{print $2}' | awk -F: '{print $2}'
+}                                                                               
+                                                                                
+### Initialize CSV file with headers. Change Headers to appropriate rack                                        
+echo "Index, Time, BD13-A, BD13-B, BD13 Total" > "$OUTPUT_FILE"                 
+                                                                                
+### Record power usage every 30 seconds. Change 'i<' to match the 'DURATION'                                     
+for ((i=0; i<10; i++))                                                          
+do                                                                              
+  TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")                                        
+  POWER_A=$(get_a_power)                                                        
+  POWER_B=$(get_b_power)                                                        
+  POWER_SUM=$(( POWER_A + POWER_B ))                                            
+                                                                                
+### Write data to CSV file                                                      
+  echo "$i, $TIMESTAMP, $POWER_A, $POWER_B, $POWER_SUM" >> "$OUTPUT_FILE"       
+### Wait for the next interval                                                  
+  sleep "$INTERVAL"                                                             
+done                                                                            
+                                                                                
+killall Powerpolling.sh
